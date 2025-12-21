1a9d1f4145a7cd83edefd9f1af62105a53c377c7
unknown <shylesh1640@gmail.com>
2025-12-21 19:39:50 +0530

    commit

---
+++ main.js
@@ -0,0 +1,3 @@
+console.log("Hello, World!");
+console.log("This is the main.js file.");
+console.log("Let's write some JavaScript code.");
---
+++ tutorial2.js
@@ -0,0 +1,18 @@
+// variables
+let name;
+name = "Shylesh";
+console.log("My name is " + name);
+
+
+let myticket = 100;
+let yourticket = 50;
+let totalticket = myticket + yourticket;
+console.log("Total tickets: " + totalticket);
+
+let a = 2.50;
+let b = 3;
+let sum = a + b;
+console.log("Sum: " + sum);
+
+let Shylesh = true;
+console.log("Is Shylesh a student? " + Shylesh);
