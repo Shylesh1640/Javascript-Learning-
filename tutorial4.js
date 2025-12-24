a24204d5a81b280a217da151b424fa5c62c236b8
unknown <shylesh1640@gmail.com>
2025-12-24 22:32:10 +0530

    day 4

---
+++ tutorial4.js
@@ -0,0 +1,35 @@
+// let we = prompt("Enter your weight in kilograms:");
+// console.log("Your weight is " + we + " kg.");
+
+let movie = "Inception";
+console.log(movie);
+console.log(`My favorite movie is ${movie}.`);
+
+// mini project 
+
+
+function villa(){
+    console.log("Welcome to the villa!");
+}
+
+villa();
+
+function sum(){
+    console.log(25 + 35);
+}
+
+sum();
+
+//default prameters
+
+function greet(name = "Guest"){
+    console.log(`Hello, ${name}!`);
+}
+
+
+greet("Shylesh");
+
+greet("Alice");
+
+//get element by id
+
