cb2d8eeb8de577d164a900f47301dfc6b586c3b6
unknown <shylesh1640@gmail.com>
2025-12-26 08:49:30 +0530

    Create Arithematic.js

---
+++ Arithematic.js
@@ -0,0 +1,17 @@
+//Syntax
+
+//if else statement
+
+let age = 20;
+
+if (age < 18) {
+    console.log("You are a minor.");
+}
+else {
+    console.log("You are an adult.");
+}
+
+//ternary operator
+
+let access = (age >= 18) ? "Granted" : "Denied";
+console.log("Access: " + access);
