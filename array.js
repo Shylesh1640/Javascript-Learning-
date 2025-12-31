bdbda03b516266eacfd417725487e16da72cc5c1
unknown <shylesh1640@gmail.com>
2025-12-31 21:33:26 +0530

    Update array.js

--- array.js
+++ array.js
@@ -10,3 +10,14 @@ fruits.shift();
 console.log(fruits); // Output: ["apple","pomegranate", "banana", "cherry","papaya"]
 console.log(fruits.includes("banana")); // Output: true
 console.log(fruits.indexOf("cherry")); // Output: 3
+fruits.splice(1,2); // Removes "pomegranate" and "banana"
+console.log(fruits); // Output: ["apple", "cherry","papaya"]
+fruits.splice(0,1,"rambutan"); // Replaces "apple" with "rambutan"
+console.log(fruits); // Output: ["rambutan","cherry","papaya"]
+let vegetables = ["carrot", "broccoli", "spinach", "potato","cabbage"];
+vegetables.sort();
+console.log(vegetables);
+let numbers = [5, 2, 9, 1, 5, 6];
+numbers.sort();
+numbers.reverse();
+console.log(numbers); // Output: [9, 6, 5, 5, 2, 1]
