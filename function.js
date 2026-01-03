4bc0fbe2a3d0008ae04143a9892b7be98995806a
unknown <shylesh1640@gmail.com>
2026-01-03 17:19:49 +0530

    Create function.js

---
+++ function.js
@@ -0,0 +1,19 @@
+//normal function
+// function name(){
+//     console.log("Shylesh");
+// }
+// name();
+
+// const name = function(){
+//     console.log("Shylesh");
+// };
+// name();
+
+//arrow function
+// const name = () =>console.log("Shylesh");
+// name();
+
+// const hero = (name,age) =>console.log(`Name: ${name}, Age: ${age}`);
+// hero("Shylesh", 25);
+
+// we can also use setbracket in arrow function
