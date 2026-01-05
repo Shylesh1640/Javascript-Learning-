11529096c3ce4022e2bb48335312a45ca13bb522
unknown <shylesh1640@gmail.com>
2026-01-05 18:46:30 +0530

    Create Hoisting.js

---
+++ Hoisting.js
@@ -0,0 +1,12 @@
+// var gandhiji = "Father of our nation";
+// console.log(gandhiji); // Output: Father of our nation
+// var gandhiji = "Father of our nation";
+// console.log(gandhiji); // Output: Father of our nation
+
+// console.log(gandhiji); // Output: Father of our nation
+// let gandhiji = "Father of our nation";
+
+// console.log(gandhiji); // Output: Father of our nation
+// const gandhiji = "Father of our nation";
+
+//let and const also hoist the variable declarations to the top of their block scope, but they do not initialize them. Accessing them before the declaration results in a ReferenceError due to the temporal dead zone (TDZ).
